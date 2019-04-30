1b6caabc92b0c062b466f310033d935ec7a58aca
shaoquan93 <chewshaoquan@gmail.com>
2019-04-30 17:17:57 +0800

    EDH sample app initial upload

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# edh-tuo-demo-app
-EDH G2B Tell-Us-Once Demo Application
+## MyInfoBiz Demo App Setup
+
+### 1.1 Install Node and NPM
+
+In order for the demo application to run, you will need to install Node and NPM.
+
+Follow the instructions given by the links below depending on your OS.
+
+-   [Install Node and NPM for Windows](http://blog.teamtreehouse.com/install-node-js-npm-windows)
+-   [Install Node and NPM for Linux](http://blog.teamtreehouse.com/install-node-js-npm-linux)
+-   [nstall Node and NPM for Mac](http://blog.teamtreehouse.com/install-node-js-npm-mac)
+
+### 1.2 Run NPM install
+
+Run the following command in the folder you unzipped the application:
+
+    npm install
+
+### 1.3 Start the Application
+
+**For Linux/MacOS**
+
+Execute the following command to start the application:
+
+      ./start.sh
+
+**For Windows**
+
+Execute the following command to start the application:
+
+      .\start.bat
+
+**Access the Application on Your Browser**
+You should be able to access the sample application via the following URL:
+
+    http://localhost:3001
+
+![Demo Screenshot](screenshot_main.png)
+![Demo Screenshot](screenshot_form.png)
+
+* * *
+
+### v1.0.0 APIs (LATEST)
+
+**For Linux/MacOS**
+
+Edit the `start.sh`. Look for `SANDBOX ENVIRONMENT`, Comment out these configurations,
+
+    # SANDBOX ENVIRONMENT (no PKI digital signature)
+    # export AUTH_LEVEL=L0
+    # export EDH_API_ENTITY='https://sandbox.api.edh.gov.sg/gov/v1/entity-sample'
+
+Look for `TEST ENVIRONMENT`, uncomment these configurations,
+
+    # TEST ENVIRONMENT (with PKI digital signature)
+    export AUTH_LEVEL=L2
+    export EDH_API_ENTITY='https://test.api.edh.gov.sg/gov/v1/entity'
+
+Execute the following command to start the application:
+
+      ./start.sh
+
+**For Windows**
+
+Edit the `start.bat`. Look for `SANDBOX ENVIRONMENT`, comment out these configurations,
+
+    rem SANDBOX ENVIRONMENT (no PKI digital signature)
+    rem set AUTH_LEVEL=L0
+    rem set export EDH_API_ENTITY=https://sandbox.api.edh.gov.sg/gov/v1/entity-sample
+
+Look for `TEST ENVIRONMENT`, uncomment these configurations,
+
+    rem TEST ENVIRONMENT (with PKI digital signature)
+    set AUTH_LEVEL=L2
+    set EDH_API_ENTITY=https://test.api.edh.gov.sg/gov/v1/entity
+
+Execute the following command to start the application:
+
+      .\start.bat
+
+* * *
+
+## Reporting issues
+
+You may contact <mailto:support@myinfo.gov.sg> for any other technical issues, and we will respond to you within 5 working days.
---
+++ routes/index.js
@@ -0,0 +1,225 @@
+var express = require('express');
+var router = express.Router();
+
+const restClient = require('superagent-bluebird-promise');
+const path = require('path');
+const url = require('url');
+const util = require('util');
+const Promise = require('bluebird');
+const _ = require('lodash');
+const querystring = require('querystring');
+const securityHelper = require('../lib/security/security');
+const crypto = require('crypto');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
+// ####################
+// Setup Configuration
+// ####################
+
+// LOADED FRON ENV VARIABLE: public key from EDH given to you during onboarding for RSA digital signature (JWS verification)
+var _publicCertContent = process.env.EDH_SIGNATURE_CERT_PUBLIC_CERT;
+// LOADED FRON ENV VARIABLE: your private key for RSA digital signature
+var _privateKeyContent = process.env.DEMO_APP_SIGNATURE_CERT_PRIVATE_KEY;
+// LOADED FRON ENV VARIABLE: your client_id provided to you during onboarding
+var _clientId = process.env.EDH_APP_CLIENT_ID;
+// LOADED FRON ENV VARIABLE: your client_secret provided to you during onboarding
+var _clientSecret = process.env.EDH_APP_CLIENT_SECRET;
+// redirect URL for your web application
+var _redirectUrl = process.env.EDH_APP_REDIRECT_URL;
+// default realm for your web application
+var _realm = process.env.EDH_APP_REALM;
+
+// URLs for EDH APIs
+var _authLevel = process.env.AUTH_LEVEL;
+
+var _entityApiUrl = process.env.EDH_API_ENTITY;
+
+// Requested attributes
+
+var _attributes = "entitytype,basic-profile,addresses,history,financials,capitals,declarations,charges,shareholders,appointments,licences,grants"
+
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../views/html/index.html'));
+});
+
+// callback function - directs back to home page
+router.get('/callback', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../views/html/index.html'));
+});
+
+// function for getting environment variables to the frontend
+router.get('/getEnv', function(req, res, next) {
+  if (_clientId == undefined || _clientId == null)
+    res.jsonp({
+      status: "ERROR",
+      msg: "client_id not found"
+    });
+  else
+    res.jsonp({
+      status: "OK",
+      clientId: _clientId,
+      redirectUrl: _redirectUrl,
+      attributes: _attributes,
+      authLevel: _authLevel
+    });
+});
+
+// function for frontend to call backend
+router.post('/getEntityData', function(req, res, next) {
+  // get variables from frontend
+  var uen = req.body.uen;
+  console.log("uen:" + uen);
+  callEntityAPI(uen, res);
+});
+
+function callEntityAPI(uen, res) {
+
+  console.log("uen:".blue + uen);
+
+  // **** CALL ENTITY API ****
+  // Call Entity API using uen
+  var request = createEntityRequest(uen);
+
+  // Invoke asynchronous call
+  request
+    .buffer(true)
+    .end(function(callErr, callRes) {
+      if (callErr) {
+        console.log("Error from Entity API:".red);
+        console.log(callErr.status);
+        console.log(callErr.response.req.res.text);
+        res.jsonp({
+          status: "ERROR",
+          msg: callErr
+        });
+      } else {
+        // SUCCESSFUL
+        var data = {
+          body: callRes.body,
+          text: callRes.text
+        };
+
+        var entityData = data.text;
+        if (entityData == undefined || entityData == null) {
+          res.jsonp({
+            status: "ERROR",
+            msg: "ENTITY DATA NOT FOUND"
+          });
+        } else {
+          if (_authLevel == "L0") {
+            entityData = JSON.parse(entityData);
+
+            console.log("Entity Data :".green);
+            console.log(JSON.stringify(entityData));
+            // successful. return data back to frontend
+            res.jsonp({
+              status: "OK",
+              text: entityData
+            });
+
+          } else if (_authLevel == "L2") {
+            console.log("Response from Entity API:".green);
+            console.log(entityData);
+            // header.encryptedKey.iv.ciphertext.tag
+            var jweParts = entityData.split(".");
+
+            securityHelper.decryptJWE(jweParts[0], jweParts[1], jweParts[2], jweParts[3], jweParts[4], _privateKeyContent)
+              .then(entityData => {
+                if (entityData == undefined || entityData == null)
+                  res.jsonp({
+                    status: "ERROR",
+                    msg: "INVALID DATA OR SIGNATURE FOR ENTITY DATA"
+                  });
+
+                console.log("Entity Data (JWS):".green);
+                console.log(JSON.stringify(entityData));
+
+                var decodedEntityData = securityHelper.verifyJWS(entityData, _publicCertContent);
+
+                if (decodedEntityData == undefined || decodedEntityData == null) {
+                  res.jsonp({
+                    status: "ERROR",
+                    msg: "INVALID DATA OR SIGNATURE FOR ENTITY DATA"
+                  })
+                }
+
+                console.log("Entity Data (Decoded):".green);
+                console.log(JSON.stringify(decodedEntityData));
+                // successful. return data back to frontend
+
+                res.jsonp({
+                  status: "OK",
+                  text: decodedEntityData
+                });
+              })
+              .catch(error => {
+                console.error("Error with decrypting JWE: %s".red, error);
+              })
+          } else {
+            throw new Error("Unknown Auth Level");
+          }
+        } // end else
+      }
+    }); // end asynchronous call
+
+}
+
+// function to prepare request for ENTITY API
+function createEntityRequest(uen) {
+  console.log("*******************************".green);
+  console.log("**** Create Entity Request ****".green);
+  console.log("*******************************".green);
+  var url = _entityApiUrl + "/" + uen;
+  var cacheCtl = "no-cache";
+  var method = "GET";
+  var request = null;
+  // assemble params for Entity API
+  var strParams = "client_id=" + _clientId +
+    "&attributes=" + _attributes;
+  var params = querystring.parse(strParams);
+
+  // assemble headers for Entity API
+  var strHeaders = "Cache-Control=" + cacheCtl;
+  var headers = querystring.parse(strHeaders);
+  var authHeaders;
+
+  // Sign request and add Authorization Headers
+  authHeaders = securityHelper.generateAuthorizationHeader(
+    url,
+    params,
+    method,
+    "", // no content type needed for GET
+    _authLevel,
+    _clientId,
+    _privateKeyContent,
+    _clientSecret,
+    _realm
+  );
+  if (!_.isEmpty(authHeaders)) {
+    _.set(headers, "Authorization", authHeaders);
+  }
+
+  console.log("Request Header for Entity API:".green);
+  console.log(JSON.stringify(headers));
+
+  // invoke Entity API
+  var request = restClient.get(url);
+
+  // Set headers
+  if (!_.isUndefined(headers) && !_.isEmpty(headers))
+    request.set(headers);
+
+  // Set Params
+  if (!_.isUndefined(params) && !_.isEmpty(params))
+    request.query(params);
+  console.log("Sending Entity Request >>>".green);
+  return request;
+}
+
+module.exports = router;
