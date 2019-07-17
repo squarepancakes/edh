fdab4b2ecc5eb03eab55c3cb6a3381e0c43b541f
shaoquan93 <chewshaoquan@gmail.com>
2019-07-17 18:06:13 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,12 +3,7 @@ set EDH_APP_CLIENT_ID=STG2-EDH-SELF-TEST
 set DEMO_APP_SIGNATURE_CERT_PRIVATE_KEY=./ssl/STG2-EDH-SELF-TEST.pem
 set EDH_SIGNATURE_CERT_PUBLIC_CERT=./ssl/stg-auth-signing-public.pem
 
-rem Myinfo Biz
-set EDH_APP_CLIENT_SECRET=44d953c796cccebcec9bdc826852857ab412fbe2
-
 set EDH_APP_REDIRECT_URL=http://localhost:3001/callback
-set EDH_APP_REALM=http://localhost:3001
-
 
 rem SANDBOX ENVIRONMENT (no PKI digital signature)
 set AUTH_LEVEL=L0
