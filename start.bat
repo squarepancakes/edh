d10a4cbd47aea5c5da5e3920b182e621923e2ece
shaoquan93 <chewshaoquan@gmail.com>
2019-12-27 15:51:47 +0800

    new staging public cert

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @ECHO off
 set EDH_APP_CLIENT_ID=STG2-EDH-SELF-TEST
 set DEMO_APP_SIGNATURE_CERT_PRIVATE_KEY=./ssl/STG2-EDH-SELF-TEST.pem
-set EDH_SIGNATURE_CERT_PUBLIC_CERT=./ssl/stg-auth-signing-public.pem
+set EDH_SIGNATURE_CERT_PUBLIC_CERT=./ssl/staging_myinfo_public_cert.pem
 
 set EDH_APP_REDIRECT_URL=http://localhost:3001/callback
 
